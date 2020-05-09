9a7e31eae0f1636403b27fe45e56f75ee9dd8108
Mr. Subei <48648114+name365@users.noreply.github.com>
2020-05-09 20:42:38 +0800

    java

---
+++ day18-day30：IDEA2020/day03/src/git/StringTest.java
@@ -0,0 +1,22 @@
+package git;
+
+/**
+ * 一道面试题
+ * @author subei
+ * @create 2020-05-09 11:50
+ */
+public class StringTest {
+    String str = new String("good");
+    char[] ch = { 't', 'e', 's', 't' };
+
+    public void change(String str, char ch[]) {
+        str = "test ok";
+        ch[0] = 'b';
+    }
+    public static void main(String[] args) {
+        StringTest ex = new StringTest();
+        ex.change(ex.str, ex.ch);
+        System.out.println(ex.str);//good
+        System.out.println(ex.ch);//best
+    }
+}
---
+++ day18-day30：IDEA2020/day03/src/github/Person.java
@@ -0,0 +1,20 @@
+package github;
+
+/**
+ * @author subei
+ * @create 2020-05-09 15:20
+ */
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+}
